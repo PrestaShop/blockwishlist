62ad2fe3517b0ccc6fbd16c7c7c3cdb332819d9f
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2014-10-14 17:06:11 +0200

    // Version updated to v1.1.5

--- blockwishlist.php
+++ blockwishlist.php
@@ -37,7 +37,7 @@ class BlockWishList extends Module
 	{
 		$this->name = 'blockwishlist';
 		$this->tab = 'front_office_features';
-		$this->version = '1.1.4';
+		$this->version = '1.1.5';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
