d1e65b13c55f319ac93812e1832eb274dbe02e3c
Rémi Gaillard <remi.gaillard@prestashop.com>
2014-12-31 11:18:56 +0100

    // Version updated to v1.1.9

--- blockwishlist.php
+++ blockwishlist.php
@@ -39,7 +39,7 @@ class BlockWishList extends Module
 	{
 		$this->name = 'blockwishlist';
 		$this->tab = 'front_office_features';
-		$this->version = '1.1.8';
+		$this->version = '1.1.9';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
