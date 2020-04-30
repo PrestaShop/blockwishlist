c9307c5be68b13de381cdbe6d42e94475a0b6150
Valentin Szczupak <valentin.szczupak@prestashop.com>
2020-04-30 11:15:31 +0200

    Add loaders

--- _dev/front/js/components/List/List.vue
+++ _dev/front/js/components/List/List.vue
@@ -17,43 +17,61 @@
  * International Registered Trademark & Property of PrestaShop SA
  *-->
 <template>
-  <ul class="wishlist-list">
-    <li class="wishlist-list-item" :key="list.id" v-for="list of items">
-      <a
-        class="wishlist-list-item-title"
-        href="http://localhost/prestashop/index.php?fc=module&module=blockwishlist&controller=productslist&id_lang=1"
-      >
-        {{ list.title }}
-        <span>({{ list.numbersProduct }})</span>
-      </a>
-
-      <div class="wishlist-list-item-right">
-        <a @click="togglePopup(list.id)" class="wishlist-list-item-actions">
-          <i class="material-icons">more_vert</i>
-        </a>
-
-        <div
-          class="dropdown-menu show"
-          v-if="activeDropdowns.includes(list.id)"
+  <div class="wishlist-list-container">
+    <ul class="wishlist-list" v-if="items.length > 0">
+      <li class="wishlist-list-item" :key="list.id" v-for="list of items">
+        <a
+          class="wishlist-list-item-title"
+          href="http://localhost/prestashop/index.php?fc=module&module=blockwishlist&controller=productslist&id_lang=1"
         >
-          <a @click="toggleRename(list.id, list.title)">{{ renameText }}</a>
-          <a @click="toggleShare(list.id)">{{ shareText }}</a>
-        </div>
-
-        <a @click="toggleDelete(list.id, list.title)">
-          <i class="material-icons">delete</i>
+          {{ list.title }}
+          <span>({{ list.numbersProduct }})</span>
         </a>
-      </div>
-    </li>
-  </ul>
+
+        <div class="wishlist-list-item-right">
+          <a @click="togglePopup(list.id)" class="wishlist-list-item-actions">
+            <i class="material-icons">more_vert</i>
+          </a>
+
+          <div
+            class="dropdown-menu show"
+            v-if="activeDropdowns.includes(list.id)"
+          >
+            <a @click="toggleRename(list.id, list.title)">{{ renameText }}</a>
+            <a @click="toggleShare(list.id)">{{ shareText }}</a>
+          </div>
+
+          <a @click="toggleDelete(list.id, list.title)">
+            <i class="material-icons">delete</i>
+          </a>
+        </div>
+      </li>
+    </ul>
+
+    <ContentLoader
+      v-if="items.length <= 0"
+      class="wishlist-list-loader"
+      height="105"
+    >
+      <rect x="0" y="12" rx="3" ry="0" width="100%" height="11" />
+      <rect x="0" y="36" rx="3" ry="0" width="100%" height="11" />
+      <rect x="0" y="60" rx="3" ry="0" width="100%" height="11" />
+      <rect x="0" y="84" rx="3" ry="0" width="100%" height="11" />
+    </ContentLoader>
+  </div>
 </template>
 
 <script>
+  import { ContentLoader } from 'vue-content-loader';
+
   /**
    * Dumb component to display the list of Wishlist on a page
    */
   export default {
     name: 'List',
+    components: {
+      ContentLoader
+    },
     data() {
       return {
         activeDropdowns: []
@@ -134,6 +152,11 @@
   .wishlist {
     &-list {
       margin-bottom: 0;
+
+      &-loader {
+        padding: 0 20px;
+        width: 100%;
+      }
 
       &-item {
         display: flex;
