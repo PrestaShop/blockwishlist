6f98d4105073c609c88ba2a6a79e3f4fb365a416
Valentin Szczupak <valentin.szczupak@prestashop.com>
2020-06-29 15:30:35 +0200

    Revert graphql tests

--- tests/UI/Delete.spec.js
+++ tests/UI/Delete.spec.js
@@ -1,31 +1,8 @@
-import Vue from 'vue';
-import {shallowMount, mount, createLocalVue} from '@vue/test-utils';
+import {shallowMount} from '@vue/test-utils';
 import Delete from '@components/Delete/Delete.vue';
 import EventBus from '@components/EventBus';
-import {addMockFunctionsToSchema} from 'graphql-tools';
-import schema from './mockSchema';
-import {graphql} from 'graphql';
-
-const removeFromList = `
-  mutation {
-    removeFromList(listId: 1, productId: 1, productAttributeId: 1, url: "#") {
-      success
-      message
-    }
-  }
-`;
 
 describe('Delete.vue', () => {
-  let localVue;
-  beforeEach(() => {
-    localVue = createLocalVue();
-
-    addMockFunctionsToSchema({
-      schema,
-      preserveResolvers: true
-    });
-  });
-
   it(`should'nt be hidden if it receive the event showDelete`, async () => {
     const wrapper = shallowMount(Delete);
 
@@ -38,13 +15,5 @@ describe('Delete.vue', () => {
     });
 
     expect(wrapper.vm.isHidden).toBe(false);
-  });
-
-  it(`should be hidden after accepting`, async () => {
-    const wrapper = shallowMount(Delete);
-
-    const result = await graphql(schema, removeFromList);
-
-    expect(result.data.removeFromList.success).toBe(true);
   });
 });
--- tests/UI/mockSchema.js
+++
@@ -1,8 +0,0 @@
-import {makeExecutableSchema} from 'graphql-tools';
-import schema from '@graphqlFiles/types';
-import resolvers from './mockResolvers';
-
-export default makeExecutableSchema({
-  typeDefs: schema,
-  resolvers
-});
