333b9f01c4145202a398a1beebe12921ce6c3fe0
pablopolyte <pablo.polyte@prestashop.com>
2020-08-19 09:57:56 +0200

    csfixer

--- controllers/front/action.php
+++ controllers/front/action.php
@@ -7,7 +7,7 @@
  * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/AFL-3.0jhggh
+ * https://opensource.org/licenses/AFL-3.0
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@prestashop.com so we can send you a copy immediately.
@@ -99,7 +99,7 @@ class BlockWishListActionModuleFrontController extends ModuleFrontController
 
         // TODO: add product to stats
 
-        Hook::exec('ActionWishlistAddProducts',[
+        Hook::exec('ActionWishlistAddProducts', [
             'idWishlist' => $idWishList,
             'customerId' => $this->context->customer->id,
             'idProduct' => $id_product,
