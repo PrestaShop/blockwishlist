9c6c570c3bdbd4c297b27d1d7a08310eaa96ed46
GoT <PierreRambaud@users.noreply.github.com>
2021-02-03 17:41:12 +0100

    Update src/Grid/Data/CurrentYearStatisticsGridDataFactory.php

--- src/Grid/Data/CurrentYearStatisticsGridDataFactory.php
+++ src/Grid/Data/CurrentYearStatisticsGridDataFactory.php
@@ -14,7 +14,6 @@ class CurrentYearStatisticsGridDataFactory extends BaseGridDataFactory implement
 
     public function getData(SearchCriteriaInterface $searchCriteria)
     {
-        $shop_id = $this->shopId;
         if ($this->cache->contains(self::CACHE_KEY_STATS_CURRENT_YEAR . $shop_id)) {
             $results = $this->cache->fetch(self::CACHE_KEY_STATS_CURRENT_YEAR . $shop_id);
         } else {
