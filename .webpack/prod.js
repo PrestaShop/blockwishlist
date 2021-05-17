dab49e586195f978263f747efa67d143a54eec9e
NeOMakinG <valentin.szczupak@prestashop.com>
2021-05-17 18:02:53 +0200

    Run lint

--- .webpack/prod.js
+++ .webpack/prod.js
@@ -9,27 +9,26 @@ const common = require('./common.js');
  *
  */
 
-const prodConfig = () =>
-  merge(common, {
-    stats: 'minimal',
-    optimization: {
-      minimizer: [
-        new TerserPlugin({
-          sourceMap: true,
-          terserOptions: {
-            output: {
-              comments: /@license/i
-            }
+const prodConfig = () => merge(common, {
+  stats: 'minimal',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true,
+        terserOptions: {
+          output: {
+            comments: /@license/i,
           },
-          extractComments: false
-        })
-      ]
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('production')
-      })
-    ]
-  });
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+  ],
+});
 
 module.exports = prodConfig;
