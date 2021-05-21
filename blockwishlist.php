394ae19ff36e3671a02d906f755d9c1872695f82
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2021-05-21 09:46:23 +0200

    Bump version 2.0.1

--- blockwishlist.php
+++ blockwishlist.php
@@ -69,7 +69,7 @@ class BlockWishList extends Module
     {
         $this->name = 'blockwishlist';
         $this->tab = 'front_office_features';
-        $this->version = '2.0.0';
+        $this->version = '2.0.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
