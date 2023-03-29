cc2d57fbc2f7dde440161cc63819b0e222bd4ebc
Akram Bakhouche <akrem_slindy@hotmail.com>
2023-03-29 12:17:56 +0100

    FO / Blockwishlist - bad display when wishlist name too long #29213

--- _dev/front/js/components/List/List.vue
+++ _dev/front/js/components/List/List.vue
@@ -284,7 +284,7 @@
 
         &-right {
           position: relative;
-          white-space: nowrap
+          white-space: nowrap;
 
           > button {
             transition: 0.25s ease-out;
@@ -326,7 +326,7 @@
           letter-spacing: 0;
           line-height: 1.375rem;
           margin-bottom: 0;
-          word-break: break-word
+          word-break: break-word;
 
           span {
             color: #7a7a7a;
