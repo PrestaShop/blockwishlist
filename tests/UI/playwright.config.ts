67592e3a00461ad4dcb001172818b707e7c58355
Progi1984 <franck.lefevre@prestashop.com>
2024-01-22 15:06:20 +0100

    Bump dependency @prestashop-core/ui-testing to 0.0.4

--- tests/UI/playwright.config.ts
+++ tests/UI/playwright.config.ts
@@ -100,13 +100,24 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    /* Capture screenshot after each test failure */
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        contextOptions: {
+          screen: {
+            width: 1680,
+            height: 900
+          }
+        },
+      },
     },
   ],
 });
