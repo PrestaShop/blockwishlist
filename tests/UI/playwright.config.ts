9e997a8443aa58c388cd51a8248bd39eb0debe4d
Progi1984 <franck.lefevre@prestashop.com>
2024-01-24 11:13:21 +0100

    Use prestashop/prestashop image

--- tests/UI/playwright.config.ts
+++ tests/UI/playwright.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 function loadGlobal(): void  {
   global.FO = {
-    URL: process.env.URL_FO || 'http://localhost:8000/',
+    URL: process.env.URL_FO || 'http://localhost/',
   };
 
   /*
