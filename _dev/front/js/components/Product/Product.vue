99510dc383d43fa6c08fceb5398d6683de1f9928
SharakPL <git@sharak.pl>
2024-02-02 00:07:26 +0100

    fix product.customizable checks

--- _dev/front/js/components/Product/Product.vue
+++ _dev/front/js/components/Product/Product.vue
@@ -116,23 +116,23 @@
       <button
         class="btn wishlist-product-addtocart"
         :class="{
-          'btn-secondary': product.customizable === '1',
-          'btn-primary': product.customizable === '0'
+          'btn-secondary': product.customizable,
+          'btn-primary': !product.customizable
         }"
-        :disabled="isDisabled || forceDisable"
+        :disabled="!product.customizable && (isDisabled || forceDisable)"
         @click="
-          product.add_to_cart_url || product.customizable === '1'
+          product.add_to_cart_url || product.customizable
             ? addToCartAction()
             : null
         "
       >
         <i
           class="material-icons shopping-cart"
-          v-if="product.customizable === '0'"
+          v-if="!product.customizable"
         >
           shopping_cart
         </i>
-        {{ product.customizable === '1' ? customizeText : addToCart }}
+        {{ product.customizable ? customizeText : addToCart }}
       </button>
 
       <button
